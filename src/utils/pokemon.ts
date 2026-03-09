4f329ac6310e4ce7ce379f9085ebe92dccf7816b
sbarczyk <szymonbarczyk@icloud.com>
2026-03-09 14:53:22 +0100

    chore: update git user info

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,5 +1,9 @@
-export const getPokemonImageUrl = (pokemonDetails: any) => {
-    return pokemonDetails?.sprites?.other?.['official-artwork']?.front_default 
-        ?? pokemonDetails?.sprites?.front_default 
-        ?? '';
-  };
+import { PokemonDetails } from "../types/pokemon";
+
+export const getPokemonImageUrl = (pokemonDetails: PokemonDetails) => {
+  return (
+    pokemonDetails?.sprites?.other?.['official-artwork']?.front_default ??
+    pokemonDetails?.sprites?.front_default ??
+    ''
+  );
+};
