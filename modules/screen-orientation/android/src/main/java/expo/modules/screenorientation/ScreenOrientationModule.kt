aba3ed45f7bf2da6e0a85770f73b36b57c807df9
sbarczyk <szymonbarczyk@icloud.com>
2026-03-16 15:25:52 +0100

    fix: necessary changes to build on android

--- modules/screen-orientation/android/src/main/java/expo/modules/screenorientation/ScreenOrientationModule.kt
+++ modules/screen-orientation/android/src/main/java/expo/modules/screenorientation/ScreenOrientationModule.kt
@@ -1,24 +1,36 @@
 package expo.modules.screenorientation
 
+import android.content.Context
 import android.content.res.Configuration
+import android.content.ComponentCallbacks
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ScreenOrientationModule : Module() {
+  
+  private val callbacks = object : ComponentCallbacks {
+    override fun onConfigurationChanged(newConfig: Configuration) {
+      val res = if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) "landscape" else "portrait"
+      sendEvent("onChange", mapOf("orientation" to res))
+    }
+    override fun onLowMemory() {}
+  }
+
   override fun definition() = ModuleDefinition {
     Name("ScreenOrientation")
-
     Events("onChange")
 
     Function("getOrientation") {
-      val isLandscape = appContext.reactContext?.resources?.configuration?.orientation == Configuration.ORIENTATION_LANDSCAPE
-      return@Function if (isLandscape) "landscape" else "portrait"
+      val orientation = appContext.reactContext?.resources?.configuration?.orientation
+      return@Function if (orientation == Configuration.ORIENTATION_LANDSCAPE) "landscape" else "portrait"
     }
 
-    OnConfigurationChanged {
-      val isLandscape = appContext.reactContext?.resources?.configuration?.orientation == Configuration.ORIENTATION_LANDSCAPE
-      val res = if (isLandscape) "landscape" else "portrait"
-      sendEvent("onChange", mapOf("orientation" to res))
+    OnStartObserving {
+      appContext.reactContext?.registerComponentCallbacks(callbacks)
+    }
+
+    OnStopObserving {
+      appContext.reactContext?.unregisterComponentCallbacks(callbacks)
     }
   }
 }
